65b0a454cfa2a510f101fa9b2452e48dda302168
Peter van Vliet <peter@actorius.nl>
2016-11-30 11:07:25 +0100

    Standaard "Hello World" resource toegevoegd.

---
+++ src/main/java/nl/actorius/resource/DefaultResource.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package nl.actorius.resource;
+
+import javax.inject.Singleton;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+/**
+ *
+ * @author Peter van Vliet
+ */
+@Singleton
+@Path("/")
+public class DefaultResource
+{
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    public String get()
+    {
+        return "Hello World";
+    }
+}
